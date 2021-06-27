1c7e6ccb21eb969b8799673750bbf1a0cb46d69f
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-06-27 15:38:35 +0500

    react custom hook finished

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,14 +1,33 @@
 import * as React from 'react';
+// if we pass object it should contains serialize, deserialize methods by deafult
+function useLocalStorageState(key, defaultValue = '',  {
+	serialize = JSON.stringify,
+	deserialize = JSON.parse
+} = {}) {
 
-function useLocalStorageState(key, initialValue = '') {
-
-	const getInitialState = () => window.localStorage.getItem(key) || initialValue;
-
-	const [ state, setState ] = React.useState(getInitialState);
-	
+	const [ state, setState ] = React.useState(() => {
+		const valueInLocalStorage = window.localStorage.getItem(key);
+		if(valueInLocalStorage){
+			// deserializing value from LocalStorage to see it without ""
+			return deserialize(valueInLocalStorage)
+		}else{
+			// check if we have computational defaultValue
+			return typeof defaultValue === 'function' ? defaultValue(): defaultValue;
+		}
+	});
+	// we don't want rerender for this value
+	const prevKeyRef = React.useRef(key);
 	React.useEffect(() => {
-		window.localStorage.setItem(key, state)
-	}, [key, state]);
+		// getting previous key value
+		const prevKey = prevKeyRef.current;
+		if(prevKey !== key){
+			window.localStorage.removeItem(prevKey);
+		}
+		// update previous key value
+		prevKeyRef.current = key;
+		// using JSON.stringify to write different types to localStorage
+		window.localStorage.setItem(key, serialize(state))
+	}, [key, serialize, state]);
 
 	return [ state, setState ]
 };
